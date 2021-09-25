4775d304c9515d7943baadea18ceaea696b6a21e
Mohammad Jihad Uddin <mjihaduddin00@outlook.com>
2021-09-25 18:18:07 -0500

    Controller Addition
    I created a new controller utilizing the Entity Framework as my ORM. This allowed me to automatically generate a large assortment of files making the process of creating a database a lot easier.

--- NM/Data/ApplicationDbContext.cs
+++ NM/Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using NM.Models;
 
 namespace NM.Data
 {
@@ -12,5 +13,6 @@ namespace NM.Data
             : base(options)
         {
         }
+        public DbSet<NM.Models.Trivia> Trivia { get; set; }
     }
 }
--- NM/Models/Trivia.cs
+++ NM/Models/Trivia.cs
@@ -7,5 +7,15 @@ namespace NM.Models
 {
     public class Trivia
     {
+        public int Id { get; set; }
+
+        public string TriviaQuestion { get; set; }
+
+        public string TriviaAnswer { get; set; }
+
+        public Trivia()
+        {
+
+        }
     }
 }
