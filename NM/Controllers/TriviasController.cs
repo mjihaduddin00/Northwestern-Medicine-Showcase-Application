67033708135cf6d2d1eb5cc96a0f2a6ae6b88192
Mohammad Jihad Uddin <mjihaduddin00@outlook.com>
2021-09-25 22:30:50 -0500

    Authorization
    I implemented Authorization to create new trivia questions simply by adding in the library and the line [Authorize].

--- NM/Controllers/TriviasController.cs
+++ NM/Controllers/TriviasController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -56,6 +57,8 @@ namespace NM.Controllers
         }
 
         // GET: Trivias/Create
+
+        [Authorize]
         public IActionResult Create()
         {
             return View();
