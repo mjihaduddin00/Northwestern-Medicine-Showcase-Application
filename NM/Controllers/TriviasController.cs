718c418e073980f40caa33e4055ecb326aa958fa
Mohammad Jihad Uddin <mjihaduddin00@outlook.com>
2021-09-25 22:16:03 -0500

    ShowSearchForm
    I created a Search portion on the navigation bar, and added it's View page into the Trivia folder.

--- NM/Controllers/TriviasController.cs
+++ NM/Controllers/TriviasController.cs
@@ -25,6 +25,12 @@ namespace NM.Controllers
             return View(await _context.Trivia.ToListAsync());
         }
 
+        // GET: Trivias/ShowSearchForm
+        public async Task<IActionResult> ShowSearchForm()
+        {
+            return View();
+        }
+
         // GET: Trivias/Details/5
         public async Task<IActionResult> Details(int? id)
         {
